81686c00e2cd936ade6b593b997ab0aed0e8686b
Dwamena-Poku Albert <albertopoku676@gmail.com>
2019-11-09 13:04:57 +0000

    alc 4.0 phase 2 project

---
+++ app/src/main/java/com/project/candystoreapp/InfoActivity.java
@@ -0,0 +1,32 @@
+package com.project.candystoreapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
+        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
+        Picasso.get().
+                load(uri).
+                into(candyStoreImageView);
+    }
+
+    // ***
+    // TODO - Task 2 - Launch the Google Maps Activity
+    // ***
+
+    // ***
+    // TODO - Task 3 - Launch the Phone Activity
+    // ***
+}
---
+++ app/src/main/java/com/project/candystoreapp/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.project.candystoreapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+import androidx.annotation.Nullable;
+
+//import PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //addPreferencesFromResource();
+    }
+}
